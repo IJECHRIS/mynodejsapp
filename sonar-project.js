5c2d3900517549b9122a00381ebf023cb26abbf8
Christopher Ijeoma <105609091+IJECHRIS@users.noreply.github.com>
2022-07-12 11:15:10 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.17.164:9000/',
+  serverUrl: 'http://54.242.238.30:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsTechDom',
-	    //'sonar.login': '8cc5903f1a3b7ec268634aaf609048c2214e8e42',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.login': '195bf1d3290ce0a31970b618d11d129497aa36c4qq',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
